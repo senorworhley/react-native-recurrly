0f6cfeb9fa6a6c2404a5c4fe2404f2130b57dc86
senorworhley <99400802+senorworhley@users.noreply.github.com>
2026-04-04 15:11:07 +0100

    feat: project setup, nativewind, routing, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import "@/global.css";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -26,15 +30,15 @@ export default function App() {
       >
         Go to Sign up
       </Link>
+      <Link href="/subscriptions/sportify">Sportify Subscription</Link>
       <Link
-        href="/subscriptions/sportify"
+        href={{
+          pathname: "/subscriptions/[id]",
+          params: { id: "claude" },
+        }}
       >
-        Sportify Subscription
+        Claude Max Subscription
       </Link>
-      <Link href={{
-        pathname: "/subscriptions/[id]",
-        params: {id: "claude"},
-      }}>Claude Max Subscription</Link>
-    </View>
+    </SafeAreaView>
   );
 }
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const Settings = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>Settings</Text>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default Settings
+export default Settings;
